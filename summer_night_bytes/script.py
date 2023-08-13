525182f78687285c362f5588ca1462046576478e
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-08-13 20:41:26 +0200

    Update script.py

--- summer_night_bytes/script.py
+++ summer_night_bytes/script.py
@@ -16,7 +16,7 @@ time_ranges = [
 # Function to categorize the time range based on hour
 def categorize_time_range(hour):
     for label, (start, end), _ in time_ranges:
-        # Check if hour falls within the range, considering wraparound for Night category
+        # Check if the hour falls within the specified range, considering wrapping around midnight
         if (start <= end and start <= hour < end) or (start > end and (start <= hour or hour < end)):
             return label
 
@@ -25,7 +25,7 @@ def generate_hex_color(min_hex, max_hex, ratio):
     r1, g1, b1 = [int(min_hex[i:i + 2], 16) for i in (1, 3, 5)]
     r2, g2, b2 = [int(max_hex[i:i + 2], 16) for i in (1, 3, 5)]
     
-    # Interpolate color components based on the ratio
+    # Calculate the RGB components of the color based on the given ratio
     r = int(r1 + ratio * (r2 - r1))
     g = int(g1 + ratio * (g2 - g1))
     b = int(b1 + ratio * (b2 - b1))
@@ -34,8 +34,8 @@ def generate_hex_color(min_hex, max_hex, ratio):
 
 # Function to analyze images in the given directory
 def analyze_images(directory):
-    image_data = []  # To store processed image data
-    skipped_images = []  # To store images without DateTimeOriginal tag
+    image_data = []  # List to store categorized image data
+    skipped_images = []  # List to store images without DateTimeOriginal tag
     
     for filename in os.listdir(directory):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -45,17 +45,16 @@ def analyze_images(directory):
                 img = Image.open(filepath)
                 exif_data = img._getexif()
                 
-                if exif_data and 36867 in exif_data:  # Check for DateTimeOriginal tag
-                    capture_time = exif_data[36867]  # Extract DateTimeOriginal tag value
-                    datetime_obj = datetime.strptime(capture_time, "%Y:%m:%d %H:%M:%S")  # Convert to datetime
+                if exif_data and 36867 in exif_data:  # Check if image has DateTimeOriginal tag
+                    capture_time = exif_data[36867]  # Extract DateTimeOriginal tag
+                    datetime_obj = datetime.strptime(capture_time, "%Y:%m:%d %H:%M:%S")
                     hour = datetime_obj.hour
-                    time_category = categorize_time_range(hour)  # Categorize time range
+                    time_category = categorize_time_range(hour)  # Categorize time based on hour
                     
                     for label, (start, end), hex_range in time_ranges:
                         if label == time_category:
                             ratio = (hour - start) / (end - start)
-                            hex_color = generate_hex_color(hex_range[0], hex_range[-1], ratio)  # Generate color
-                            # Add processed image data to the list
+                            hex_color = generate_hex_color(hex_range[0], hex_range[-1], ratio)
                             image_data.append({
                                 "image": filename,
                                 "time": datetime_obj.strftime("%H:%M"),
@@ -64,7 +63,7 @@ def analyze_images(directory):
                             })
                             break
                 else:
-                    skipped_images.append(filename)  # No DateTimeOriginal tag
+                    skipped_images.append(filename)  # Image does not contain DateTimeOriginal tag
             except (AttributeError, KeyError, ValueError) as e:
                 print(f"Error processing image {filename}: {e}")
                 pass
@@ -75,9 +74,11 @@ if __name__ == "__main__":
     input_directory = "path/to/your/images/folder"
     output_data, skipped_images = analyze_images(input_directory)
     
-    df = pd.DataFrame(output_data)  # Create DataFrame from processed image data
-    df.to_csv("output_data.csv", index=False)  # Export DataFrame to CSV file
+    # Create a DataFrame from the processed data and export it to a CSV file
+    df = pd.DataFrame(output_data)
+    df.to_csv("output_data.csv", index=False)
     
+    # Print the processed images along with any skipped images without DateTimeOriginal tag
     print("Processed images:")
     for entry in output_data:
         print(entry)
