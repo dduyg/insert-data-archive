80726b26a54859e948ed3ccc2d0171ca7eed99ae
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-08-15 14:26:44 +0200

    Update TaskTypeClassifier.py

--- code/between_the_lines/TaskTypeClassifier.py
+++ code/between_the_lines/TaskTypeClassifier.py
@@ -66,15 +66,15 @@ def get_calendar_tasks():
     # Define the calendar ID or leave it as 'primary' for the default calendar
     calendar_id = 'primary'
     
-    # Calculate the start date for the last three months accurately
+    # Calculate the start date for the last six months accurately
     today = datetime.datetime.today()
-    three_months_ago = today - relativedelta(months=3)
+    six_months_ago = today - relativedelta(months=6)
     
     # Format the dates as strings in RFC3339 format for API query
-    start_date = three_months_ago.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
+    start_date = six_months_ago.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
     end_date = today.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
     
-    # Define the query parameters to filter tasks for the last three months
+    # Define the query parameters to filter tasks for the last six months
     events_result = service.events().list(
         calendarId=calendar_id,
         singleEvents=True,
