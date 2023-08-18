73b5dad8f9d9600e0065e028ce3529bccfffd145
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-08-18 21:15:35 +0200

    Update summer_time_colorizer.py

--- code/summer_night_bytes/summer_time_colorizer.py
+++ code/summer_night_bytes/summer_time_colorizer.py
@@ -7,26 +7,26 @@ import colorsys
 # Define time ranges and corresponding hex color ranges
 time_ranges = [
     # The morning range with shades of yellow colors.
-    ("Morning", (6, 11), ['#E7D48F', '#EDDB6F', '#e2cc0d']),
+    ("Morning", (6, 12), ['#E7D48F', '#EDDB6F', '#e2cc0d']),
     
-    # The early afternoon range with shades of green colors.
-    ("Early Afternoon", (12, 14), ['#CEDCB8', '#B4F735', '#4AD83D']),
+    # The noon range with shades of green colors.
+    ("Noon", (12, 15), ['#CEDCB8', '#B4F735', '#4AD83D']),
     
-    # The late afternoon range with shades of blue colors.
-    ("Late Afternoon", (15, 17), ['#87B7B6', '#15C3C3', '#377D9B']),
+    # The afternoon range with shades of blue colors.
+    ("Afternoon", (15, 18), ['#87B7B6', '#15C3C3', '#377D9B']),
     
     # The evening range with shades of pink colors.
-    ("Evening", (18, 20), ['#EDB7CF', '#FD94CF', '#CC5CCC', '#CB21B5']),
+    ("Evening", (18, 0), ['#EDB7CF', '#FD94CF', '#CC5CCC', '#CB21B5']),
     
     # The night range with shades of purple/dark blue colors.
-    ("Night", (21, 6), ['#CFBCD0', '#93849F', '#5449C7', '#131F8F'])
+    ("Night", (0, 6), ['#CFBCD0', '#93849F', '#5449C7', '#131F8F'])
 ]
 
 # Function to categorize the time range based on hour
 def categorize_time_range(hour):
     for label, (start, end), _ in time_ranges:
         # Check if the hour falls within the specified range, considering wrapping around midnight
-        if (start <= end and start <= hour <= end) or (start > end and (start <= hour or hour <= end)):
+        if (start <= end and start <= hour < end) or (start > end and (start <= hour or hour < end)):
             return label
 
 # Function to generate a hex color based on the given range and ratio
